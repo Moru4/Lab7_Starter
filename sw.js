388644a55246f8415b040ccb85b9667d968b407b
Moru4 <91434951+Moru4@users.noreply.github.com>
2021-11-07 23:50:57 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,12 +3,29 @@
 
 const CACHE_NAME = 'lab-7-starter';
 
+
 // Once the service worker has been installed, feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
   /**
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
+  const urlsToCache = [
+    'https://moru4.github.io/Lab7_Starter/index.html',
+    'https://moru4.github.io/Lab7_Starter/index.html#ghostCookies',
+    'https://moru4.github.io/Lab7_Starter/index.html#birthdayCake',
+    'https://moru4.github.io/Lab7_Starter/index.html#chocolateChip',
+    'https://moru4.github.io/Lab7_Starter/index.html#stuffing',
+    'https://moru4.github.io/Lab7_Starter/index.html#turkey',
+    'https://moru4.github.io/Lab7_Starter/index.html#pumpkinPie'
+  ];
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 /**
@@ -21,6 +38,7 @@ self.addEventListener('activate', function (event) {
    * TODO - Part 2 Step 3
    * Create a function as outlined above, it should be one line
    */
+  event.waitUntil(clients.claim());
 });
 
 // Intercept fetch requests and store them in the cache
@@ -29,4 +47,14 @@ self.addEventListener('fetch', function (event) {
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        //Cache hit - return response
+        if(response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
